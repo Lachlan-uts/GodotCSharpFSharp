b94e4a349ac91b5069097ad9f16afb540a58f183
Lachlan-UTS <lachlan.d.wood@student.uts.edu.au>
2020-06-08 15:28:15 +1000

    Fix Pathing and local visual
    Can still be further improved, maybe by ditching the dedicated Vector2[]
    for using the line2d node, though I'm unsure as to the perf implications
        of this.
    
    Now for unit control all is needed is some form of input catching and
    for A.I all is needed is a form of basic decision making. I'm probably
        just going to do a simple switch statement for now to impliment a
        finite state machine.

--- C# experiment/PathfindingExperiment/PathfindingTilemapExperiment.cs
+++ C# experiment/PathfindingExperiment/PathfindingTilemapExperiment.cs
@@ -9,34 +9,33 @@ public class PathfindingTilemapExperiment : Node
 
     private Navigation2D pathmaker;
     private TileMap floor;
-    private Line2D visAid;
+
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        visAid = GetNode("visualAid") as Line2D;
-
+        GD.Print("doing ready on " + this.Name);
         floor = GetNode("Navigation2D/TileMap") as TileMap;
 
         pathmaker = GetNode("Navigation2D") as Navigation2D;
         Vector2 startPoint = pathmaker.GetClosestPoint(new Vector2(25,25));
         Vector2 endPoint = pathmaker.GetClosestPoint(new Vector2(350,25));
 
+        //var difDemoPath = pathmaker.GetSimplePath(startPoint,endPoint, true) as Godot.Collections.Array<Vector2>;
         Vector2[] demoPath = pathmaker.GetSimplePath(startPoint,endPoint, true);
-        visAid.Points = pathmaker.GetSimplePath(startPoint,endPoint, true);
         GD.Print(demoPath.ToString());
         foreach (var point in demoPath)
         {
             GD.Print("raw points: " + point);
             GD.Print("tilepoints: " + floor.WorldToMap(point));
         }
+        base._Ready();
     }
 
     public Vector2[] CalcPath(Vector2 start, Vector2 end)
     {
         bool optiChoice = true;
-        visAid.Points = pathmaker.GetSimplePath(start,end, optiChoice);
-        return visAid.Points;
+        return pathmaker.GetSimplePath(start,end, optiChoice);
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
--- C# experiment/PathfindingExperiment/Pawn.cs
+++ C# experiment/PathfindingExperiment/Pawn.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Threading.Tasks;
 
 public class Pawn : Area2D
 {
@@ -13,14 +14,50 @@ public class Pawn : Area2D
     private Vector2 velocity = new Vector2();
 
     [Export]
-    private Vector2 destination;
+    private Vector2 currentDestination;
 
     private PathfindingTilemapExperiment pathProvider;
-    private Vector2[] currentPath;
+    
+    private Line2D visAid;
 
+    //these 2 are used to step through a path.
+    private int currentPoint;
+    private Vector2[] currentPath;
+    public Vector2[] CurrentPath
+    {
+        get
+        {
+            return this.currentPath;
+        }
+        set
+        {
+            this.currentPath = value;
+            if (currentPath.Length > 1)
+            {
+                currentPoint = 1;
+                foreach (Vector2 point in currentPath)
+                {
+                    GD.Print("recieved path points: ", point.ToString());
+                    //visAid.AddPoint(point + this.GlobalPosition);
+                }
+                visAid.Points = value;
+                foreach (Vector2 point in visAid.Points)
+                {
+                    GD.Print("drawing path points: ", point.ToString());
+                }
+            } else
+            {
+                currentPoint = 0;
+                visAid.ClearPoints();
+            }
+        }
+    }
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        GD.Print("doing ready on " + this.Name);
+        visAid = GetNode("visualAid") as Line2D;
+        visAid.SetAsToplevel(true);
         if (GetParent() is PathfindingTilemapExperiment)
         {
             pathProvider = GetParent<PathfindingTilemapExperiment>();
@@ -28,31 +65,69 @@ public class Pawn : Area2D
         {
             GD.PrintErr("The parent was wrong!");
         }
+        //hardcoded set for now until I provide some thing to do this.
+        WorkAround();
         //destination = this.Position;
     }
 
+    private async Task WorkAround()
+    {
+        await ToSignal(pathProvider, "ready");
+        CurrentPath = pathProvider.CalcPath(this.Position, currentDestination);
+    }
 
+    private void PathToDestination(Vector2 target)
+    {
+        if (currentDestination != target && currentDestination != this.Position)
+        {
+            CurrentPath = pathProvider.CalcPath(this.Position, currentDestination);
+        }
+    }
+
+    private void WalkPath()
+    {
+        // GD.Print("point 0 before update", visAid.GetPointPosition(0).ToString());
+        visAid.SetPointPosition(0,this.GlobalPosition);
+        // GD.Print("point 0 after update", visAid.GetPointPosition(0).ToString());
+        if (this.Position.DistanceTo(CurrentPath[currentPoint]) < 1)
+        {
+            if (this.Position.DistanceTo(currentDestination) > 1)
+            {
+                currentPoint++;
+                visAid.RemovePoint(1);
+            } else
+            {
+                CurrentPath = new Vector2[0];
+            }
+        }
+    }
 
  // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-        // int currentPoint
+        if (currentPoint != 0)
+        {
+            //GD.Print("current point is: ", currentPoint.ToString());
+            velocity = this.Position.DirectionTo(currentPath[currentPoint]);
+
+            //GD.Print(velocity);
+            //GD.Print(currentPath[currentPoint]);
+            this.Position += (velocity * delta * speed);
+            WalkPath();
+        }
+        // int currentPoint 
 
         // if (currentPath[currentPath.Length-1] == destination)
-        if (destination != this.Position)
-        {
-            currentPath = pathProvider.CalcPath(this.Position, destination);
+        // if (currentDestination != this.Position)
+        // {
+        //     currentPath = pathProvider.CalcPath(this.Position, currentDestination);
             
-        }
-        if ((this.Position.DistanceSquaredTo(currentPath[0]) < 1))
-        {
-            GD.Print("bah");
-        }
-        velocity = this.Position.DirectionTo(currentPath[1]);
+        // }
+        // if ((this.Position.DistanceSquaredTo(currentPath[0]) < 1))
+        // {
+        //     GD.Print("bah");
+        // }
 
-        GD.Print(velocity);
-        GD.Print(currentPath[0]);
-        this.Position += (velocity * delta * speed);
     }
 
 }
