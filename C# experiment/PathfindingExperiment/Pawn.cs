4da9b608357e571371dd6600b23bab0299fdac61
Lachlan-UTS <lachlan.d.wood@student.uts.edu.au>
2020-06-16 09:30:39 +1000

    Create framework for GOAP AI
    Created a framework for a GOAP ai, with two states, walkaround and rest.
    However there are numerous bugs and the code is rather messy meaning I'm
    struggling to solve these issues. Notably I can't get the AI to properly
    switch between tasks as their evaluation of needs seems to have some
    problems. In addition the language is rather confusing.

--- C# experiment/PathfindingExperiment/Pawn.cs
+++ C# experiment/PathfindingExperiment/Pawn.cs
@@ -66,11 +66,12 @@ public class Pawn : Area2D
     enum NeedTraits
     {
         // Name, // Unecessary I think
-        MinimumValue,
-        MaximumValue,
+        OptimumValue,
+        WorstValue,
         //StepPoints, // Won't work as I want it to be an array or list
         DecayDirection,
-        DecayRate
+        DecayRate,
+        CurrentDirection
     }
     // The names of the various needs
     enum NeedNames
@@ -79,12 +80,18 @@ public class Pawn : Area2D
         Bordom
     }
 
+    //A tracker of the current targeted need/state?
+    private NeedNames currentTargetNeed;
+
     private Dictionary<NeedNames, float> needStates;
     //  = new Dictionary<NeedNames, float>
     // {
     //     {NeedNames.Rest, 10.0f},
     //     {NeedNames.Bordom, 0.0f}
     // };
+
+    // I'm thinking maybe I should make this a static reference
+    // Store things like current decay direction within the states dictionary...
     private Dictionary<NeedNames, Dictionary<NeedTraits, float>> needsCollections;
     //  = new Dictionary<NeedNames, Dictionary<NeedTraits, float>>()
     // {
@@ -115,35 +122,40 @@ public class Pawn : Area2D
     {
         needFrameSkip = 5;
         frameCount = 0;
+
+        // the target need
+        currentTargetNeed = NeedNames.Bordom;
+
         needStates = new Dictionary<NeedNames, float>
         {
-            {NeedNames.Rest, 10.0f},
-            {NeedNames.Bordom, 0.0f}
+            {NeedNames.Rest, 9.0f},
+            {NeedNames.Bordom, 1.0f}
         };
-
         needsCollections = new Dictionary<NeedNames, Dictionary<NeedTraits, float>>()
         {
             {
                 NeedNames.Rest,
                 new Dictionary<NeedTraits, float>
                 {
-                    {NeedTraits.MinimumValue, 0.0f},
-                    {NeedTraits.MaximumValue, 10.0f},
-                    {NeedTraits.DecayDirection, 1.0f},
-                    {NeedTraits.DecayRate, 0.1f}
+                    {NeedTraits.OptimumValue, 10.0f}, // need a better name, 
+                    {NeedTraits.WorstValue, 0.0f}, // Maybe depleted value?
+                    {NeedTraits.DecayDirection, 1.0f}, // getting less rested/increasingly tired
+                    {NeedTraits.DecayRate, 0.1f},
+                    {NeedTraits.CurrentDirection, 1.0f} // Keep this sort of "fake" static
                 }
             },
             {
                 NeedNames.Bordom,
                 new Dictionary<NeedTraits, float>
                 {
-                    {NeedTraits.MinimumValue, 0.0f},
-                    {NeedTraits.MaximumValue, 10.0f},
-                    {NeedTraits.DecayDirection, -1.0f},
-                    {NeedTraits.DecayRate, 0.05f}
+                    {NeedTraits.OptimumValue, 0.0f},
+                    {NeedTraits.WorstValue, 10.0f},
+                    {NeedTraits.DecayDirection, -1.0f}, // getting increasingly bored
+                    {NeedTraits.DecayRate, 0.2f},
+                    {NeedTraits.CurrentDirection, -1.0f}
                 }
-        },
-    };
+            }
+        };
 
 
         GD.Print("doing ready on " + this.Name);
@@ -158,14 +170,19 @@ public class Pawn : Area2D
             GD.PrintErr("The parent was wrong!");
         }
         //hardcoded set for now until I provide some thing to do this.
-        DelayedReadyWorkAround();
+        DelayedReadyWorkAround("ready");
         //destination = this.Position;
     }
 
-    private async Task DelayedReadyWorkAround()
+    private async Task DelayedReadyWorkAround(string signal)
     {
-        await ToSignal(pathProvider, "ready");
-        WanderAround(pathProvider.CalcValidArea(new Rect2(this.Position + wanderOriginOffset, wanderRectSize))); //make sure you don't subtract a negative vector you silly billy
+        // A hack to enable reuse of this function.
+        // I CAN FEEL YOUR JUDGEMENT PLZ FORGIVE!
+        if (signal == "ready")
+        {
+            await ToSignal(pathProvider, signal);
+        }
+        currentDestination = ChooseWanderDestination(pathProvider.CalcValidArea(new Rect2(this.Position + wanderOriginOffset, wanderRectSize))); //make sure you don't subtract a negative vector you silly billy
         CurrentPath = pathProvider.CalcPath(this.Position, currentDestination);
         if (CurrentPath[CurrentPath.Length - 1] != currentDestination)
         {
@@ -174,18 +191,72 @@ public class Pawn : Area2D
         GD.Print(pathProvider.CalcPoint(currentDestination));
     }
 
-    private void PathToDestination(Vector2 target)
+    private NeedNames EvaluateStates(Dictionary<NeedNames, float> currentStates)
     {
-        if (currentDestination != target && currentDestination != this.Position)
+        // I need to make all states comparable (fix the directional flips) and then evaluate them
+        // Setup a random gen to allow for some deviation, to be decided on later.
+        RandomNumberGenerator rand = new RandomNumberGenerator();
+        rand.Randomize();
+
+        // Find whichever need is further from it's optimum state and swap to it preferably.
+        var targetNeed = currentTargetNeed; // Establish the current working need to give it advantage? Allow for some need stickyness
+        foreach (NeedNames need in currentStates.Keys)
         {
-            CurrentPath = pathProvider.CalcPath(this.Position, currentDestination);
+            // I need to check distence from minimum (how close to being depleted it is) or "maximum" in the case of something like bordom
+            // In short we're checking for distance from optimum value, or distance to worst value.
+            // The further we are, the worse things are.
+            if (need != targetNeed && 
+                Math.Abs(needsCollections[need][NeedTraits.OptimumValue] - currentStates[need]) > Math.Abs(needsCollections[targetNeed][NeedTraits.OptimumValue] - currentStates[targetNeed])
+                )
+            {
+                targetNeed = need;
+            }
+        }
+        return targetNeed;
+    }
+
+    // There might be wisdom in changing this from void to a return type of state, to help keep track of things though unsure of how to do so currently.
+    private void ChooseState() // I changed the name because the pawn might remain in a state upon evaluation
+    {
+        var chosenNeed = EvaluateStates(needStates);
+
+        //Ideally I should use some sort of match function here but I'm too lazt atm
+        if (chosenNeed == NeedNames.Rest)
+        {
+            GD.Print("choose rest");
+            Rest();
+        } else
+        {
+            GD.Print("choose walk");
+            DelayedReadyWorkAround("ready");
         }
     }
 
-    private void WanderAround(Rect2 possibleArea)
+    // This function/method should allow for dictating a rest state.
+    private void Rest()
     {
-        needsCollections[NeedNames.Rest][NeedTraits.DecayRate] = 0.1f; //set the various need rates
-        needsCollections[NeedNames.Bordom][NeedTraits.DecayDirection] = 1.0f; //set the various need rates
+        needsCollections[NeedNames.Rest][NeedTraits.CurrentDirection] = needsCollections[NeedNames.Rest][NeedTraits.DecayDirection] * -1.0f; // reverse the decay
+        needsCollections[NeedNames.Bordom][NeedTraits.CurrentDirection] = needsCollections[NeedNames.Bordom][NeedTraits.DecayDirection]; // reverse the decay
+        currentDestination = this.Position;
+        // This rotation method only works if you only trigger rest on changing from a different state!
+        // DERP
+
+
+        // I maybe should pull these changes/declarations into a separatChooseState()e function, but I'm not sure how to make it shorter. :/
+        // I could maybe make these declarations dependent but that could have mistakes...
+        // List<NeedNames> needStatesKeys = new List<NeedNames>(needStates.Keys);
+        // foreach (var key in needStatesKeys)
+        // {
+        //     needsCollections[key][NeedTraits.DecayDirection] = needsCollections[key][NeedTraits.DecayDirection] * -1.0f; //This should invert the current states
+        // }
+        // needsCollections[NeedNames.Rest][NeedTraits.DecayDirection] = 1.0f; //set the various need rates
+        // needsCollections[NeedNames.Bordom][NeedTraits.DecayDirection] = 1.0f; //set the various need rates
+    }
+
+    private Vector2 ChooseWanderDestination(Rect2 possibleArea)
+    {
+        needsCollections[NeedNames.Rest][NeedTraits.CurrentDirection] = needsCollections[NeedNames.Rest][NeedTraits.DecayDirection]; // Match state of decay
+        needsCollections[NeedNames.Bordom][NeedTraits.CurrentDirection] = needsCollections[NeedNames.Bordom][NeedTraits.DecayDirection] * -1.0f; // Match state of decay
         RandomNumberGenerator rand = new RandomNumberGenerator();
         rand.Randomize();
         int[] walkableTiles = new int[] { 0 }; //hardcoded garbo for now, think more about it later
@@ -199,7 +270,8 @@ public class Pawn : Area2D
         //     (float)GD.RandRange(possibleArea.Position.x, possibleArea.End.x)
         //     ,(float)GD.RandRange(possibleArea.Position.y, possibleArea.End.y)
         //     );
-        currentDestination = pathProvider.GetValidTile(point, walkableTiles);
+        //currentDestination = pathProvider.GetValidTile(point, walkableTiles);
+        return pathProvider.GetValidTile(point, walkableTiles);
     }
     private void WalkPath()
     {
@@ -219,8 +291,8 @@ public class Pawn : Area2D
             else
             {
                 //hardcode terrible loop thingy
-                WanderAround(pathProvider.CalcValidArea(new Rect2(this.Position + wanderOriginOffset, wanderRectSize)));
-                CurrentPath = pathProvider.CalcPath(this.Position, currentDestination);
+                // currentDestination = ChooseWanderDestination(pathProvider.CalcValidArea(new Rect2(this.Position + wanderOriginOffset, wanderRectSize)));
+                // CurrentPath = pathProvider.CalcPath(this.Position, currentDestination);
                 //CurrentPath = new Vector2[0];
             }
         }
@@ -229,7 +301,7 @@ public class Pawn : Area2D
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-        if (CurrentPath.Length > 1 && needStates[NeedNames.Rest] > 0)
+        if (currentTargetNeed == NeedNames.Bordom && CurrentPath.Length > 1 && needStates[NeedNames.Rest] > 0)
         {
             //GD.Print("current point is: ", currentPoint.ToString());
             // GD.Print(currentPoint.ToString());
@@ -261,11 +333,15 @@ public class Pawn : Area2D
             List<NeedNames> needStatesKeys = new List<NeedNames>(needStates.Keys);
             foreach (var key in needStatesKeys)
             {
-                if (needStates[key] > needsCollections[key][NeedTraits.MinimumValue] && needStates[key] <= needsCollections[key][NeedTraits.MaximumValue]) // Make sure the need is within the range
+                if  (needStates[key] > Math.Min(needsCollections[key][NeedTraits.OptimumValue],needsCollections[key][NeedTraits.WorstValue]) 
+                        && needStates[key] < Math.Max(needsCollections[key][NeedTraits.OptimumValue],needsCollections[key][NeedTraits.WorstValue])
+                    ) // Make sure the need is within the range
                 {
-                    needStates[key] -= (needsCollections[key][NeedTraits.DecayRate] * needsCollections[key][NeedTraits.DecayDirection]); //adjust the need
+                    // A need should decay via minus, as most needs are depleted during decay.
+                    // So when I wish to decay I should use positive numbers and when I wish to restore I should use positive numbers.
+                    needStates[key] -= (needsCollections[key][NeedTraits.DecayRate] * needsCollections[key][NeedTraits.CurrentDirection]); //adjust the need
                 }
-                GD.Print(key.ToString(), needStates[key].ToString());
+                GD.Print(key.ToString(),": ", needStates[key].ToString());
             }
             // for (int i = 0; i < needStates.Count; i++)
             // {
@@ -281,6 +357,7 @@ public class Pawn : Area2D
             //     GD.Print(needKey.ToString(), needStates[needKey].ToString());
             // }
             frameCount = 0;
+            ChooseState();
         }
     }
 
