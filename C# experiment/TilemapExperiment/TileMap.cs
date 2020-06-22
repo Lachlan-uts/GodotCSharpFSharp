b4416ce312d3d68332361114a64cdd5e844b5aef
Lachlan-UTS <lachlan.d.wood@student.uts.edu.au>
2020-06-22 15:34:53 +1000

    Added tile remove functionality
    Tiles can now be removed which will tell their neighbours to attempt to
    grow again, if they can.
    
    Also added an additional, separate plant of grass which in total allows
    for initial emergent gameplay to be finally seen!
    
    In this commit I looked into using interfaces after learning about them
    from a friend, however they didn't satisfactorily solve the issue I was
    facing so I ended up avoiding them as I don't have sufficient time.

--- C# experiment/TilemapExperiment/TileMap.cs
+++ C# experiment/TilemapExperiment/TileMap.cs
@@ -2,13 +2,21 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
-public class TileMap : Godot.TileMap
+interface ITestInterface
 {
+    void FooBar();
+}
+
+public class TileMap : Godot.TileMap, ITestInterface
+{
+
     [Export]
     public Godot.Collections.Dictionary<int,string> nodeTileScenePaths;
     private Dictionary<int,PackedScene> loadedTiles = new Dictionary<int, PackedScene>(); //kept as an abstract node for now
+    private Dictionary<Vector2,Plant> plantTileLocations = new Dictionary<Vector2, Plant>();
     public override void _Ready()
     {
+        FooBar();
         //turn the strings into packed scenes
         foreach (int nodeTileID in nodeTileScenePaths.Keys)
         {
@@ -29,19 +37,46 @@ public class TileMap : Godot.TileMap
         }
     }
 
+    public void FooBar()
+    {
+        GD.Print("WAAAAAAAAAAAAAAAAAAAA");
+    }
+
     public new void SetCellv(Vector2 position, int tileID, bool flipX=false, bool flipY=false, bool transpose=false)
     {
         // add a terrible check to save problems for now, which probably is redundent but hey
-        // if this tile is already there, then don't try to set it again.
-        //if (this.GetCellv(position) == tileID) { return; } 
-        base.SetCellv(position,tileID,flipX,flipY,transpose);
+        // if this tile is already there, and it's node is already there, then don't try to set it again.
+        if (this.GetCellv(position) == tileID && plantTileLocations.ContainsKey(position)) { return; }
+        
+        // remove the existing plant node
+        if (plantTileLocations.ContainsKey(position))
+        {
+            plantTileLocations[position].QueueFree();
+            plantTileLocations.Remove(position);
+        }
+
+        // I should test to see if there is a tilenode existing in the spot already
+        base.SetCellv(position,tileID,flipX,flipY,transpose); //do the tilemap change
+
+        // Now I want to alert everything that can be alerted nearby
+        foreach (Vector2 direction in new Vector2[] { Vector2.Up, Vector2.Down, Vector2.Left, Vector2.Right })
+        {
+            Plant neighbour;
+            if (plantTileLocations.TryGetValue(position+direction,out neighbour))
+            {
+                neighbour.UpdateAdjacentTile(position, tileID);
+            }
+        }
         if (loadedTiles.ContainsKey(tileID))
         {
-            
-            var tileNode = loadedTiles[tileID].Instance() as Area2D;
-            tileNode.Position = this.MapToWorld(position);
-            tileNode.Name = position.ToString();
-            AddChild(tileNode);
+            plantTileLocations.Add(position,loadedTiles[tileID].Instance() as Plant);
+            plantTileLocations[position].Position = this.MapToWorld(position);
+            plantTileLocations[position].Name = position.ToString();
+            AddChild(plantTileLocations[position]);
+            // var tileNode = loadedTiles[tileID].Instance() as Plant; //Hardcode for now
+            // tileNode.Position = this.MapToWorld(position);
+            // tileNode.Name = position.ToString();
+            // AddChild(tileNode);
         }
     }
 
