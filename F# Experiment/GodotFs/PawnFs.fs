98f85bc95899b8ddb7cbe094310c419e40af542b
Lachlan-UTS <lachlan.d.wood@student.uts.edu.au>
2020-08-25 15:19:26 +1000

    Create the initial F# Project via a basic C# shim system.
    Amended because I don't know enough to add these files to the .gitignore
    file.

---
+++ F# Experiment/GodotFs/PawnFs.fs
@@ -0,0 +1,10 @@
+﻿namespace GodotFs
+ 
+open Godot
+
+type PawnFs() =
+    inherit Node()
+ 
+    override this._Ready() = 
+        GD.Print("Hello from F#!")
+        GD.Print("I think this works")
