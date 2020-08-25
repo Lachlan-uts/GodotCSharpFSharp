f2320a0cc4226f3318337e2ee17efcbbe3d9d18f
Lachlan-UTS <lachlan.d.wood@student.uts.edu.au>
2020-08-25 15:19:26 +1000

    Create the initial F# Project via a basic C# shim system.

---
+++ F# Experiment/GodotFs/obj/Debug/.NETFramework,Version=v4.7.AssemblyAttributes.fs
@@ -0,0 +1,3 @@
+namespace Microsoft.BuildSettings
+                [<System.Runtime.Versioning.TargetFrameworkAttribute(".NETFramework,Version=v4.7", FrameworkDisplayName=".NET Framework 4.7")>]
+                do ()
---
+++ F# Experiment/Pawn.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+using System;
+
+using GodotFs;
+
+public class Pawn : PawnFs
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
