958e014e6f8e0eabe6af596a7c0f91f307ddff02
Lachlan-UTS <lachlan.d.wood@student.uts.edu.au>
2020-09-08 20:10:55 +1000

    Make F# get random lazy
    This should hopefully make randomness, random and not deterministic
    again, which was a problem in the prior commit.

--- C# experiment/GodotFs/PathfindingFs.fs
+++ C# experiment/GodotFs/PathfindingFs.fs
@@ -1,17 +1,20 @@
 ﻿module PathProvider
 open Godot
 
-let randy = new RandomNumberGenerator()
-randy.Randomize()
-
+let randy = 
+    lazy(
+        new RandomNumberGenerator()
+        :?> RandomNumberGenerator
+    )
+    
 let getClosestValidTile (nav2D : Lazy<Navigation2D>) point =
     nav2D.Value.GetClosestPoint(point)
 
 
 let getRandomPointFromArea (possibleArea : Rect2) (validTiles : int[]) =
-    randy.Randomize()
+    randy.Value.Randomize()
     new Vector2(
-        randy.RandfRange(possibleArea.Position.x, possibleArea.End.x),
-        randy.RandfRange(possibleArea.Position.y, possibleArea.End.y)
+        randy.Value.RandfRange(possibleArea.Position.x, possibleArea.End.x),
+        randy.Value.RandfRange(possibleArea.Position.y, possibleArea.End.y)
     )
 
