eac0d8b887f9a8c4bea045214e0509b211a88611
Lachlan-UTS <lachlan.d.wood@student.uts.edu.au>
2020-09-22 14:23:08 +1000

    Expand NeedSystem and Start on TaskSystem
    I've got most of the need functions written, now I'm just working on
    making the tasks to relate to and operate off them.
    
    I'm not certain but I think I might look into make a queue or such for
    tasks and then just operate on/through that list to change/use tasks.
    
    I need to update the getPriorityNeed function to make sure it returns a
    NeedName, not a float (or maybe a tuple of the two?)
    
    Once I have that, all I need to do is then make the changeTask function
    and I should have all the essential building blocks. Then comes the
    challenge or task of seeing what else I need to build/shim to use this
    to replace the existing C# system.

--- C# experiment/GodotFs/Logic Modules/NeedSystem.fs
+++ C# experiment/GodotFs/Logic Modules/NeedSystem.fs
@@ -4,13 +4,15 @@ type DecayDirection = Up | Down
 
 type NeedName = Rest | Boredom
 
+type Gaining = bool
+
 type Need =
     {
         //NeedName: NeedName;
         CurrentValue: float;
         Maxi: float; // also the optimum value
         Mini: float; // also the worst value
-        Gaining: bool;
+        GainState: Gaining;
         DecayDirection: DecayDirection; // Only a hint for the visual system
         ChangeRate: float;
     }
@@ -20,8 +22,8 @@ type Need =
 
 let changeNeedDir state (need: Need) =
     match state with
-    | x when need.Gaining = x -> need
-    | _ -> {need with Gaining = state}
+    | x when need.GainState = x -> need
+    | _ -> {need with GainState = state}
 
 let changeNeedMag rate (need: Need) =
     {need with ChangeRate = rate}
@@ -31,7 +33,7 @@ let changeNeedDirAndMag state rate (need: Need) =
 
 /// This is for a single need, will need adjustment for use with the map
 let updateNeed (need: Need) =
-    match need.Gaining with
+    match need.GainState with
     | true when need.CurrentValue <> need.Maxi ->
         let newValue = need.CurrentValue + need.ChangeRate
         if newValue > need.Maxi then
@@ -46,6 +48,7 @@ let updateNeed (need: Need) =
             {need with CurrentValue = newValue}
     | _ -> need
 
+/// From here in the file I think I'll work on map (collection of need) functions
 
 let updateNeedMap (needMap : Map<NeedName,Need>) =
     Map.map (fun (name : NeedName) need -> updateNeed need) needMap
@@ -54,11 +57,20 @@ let mapBounce needMap =
     Map.map (fun (name : NeedName) (need : Need) ->
         match need.CurrentValue with
         | x when x = need.Maxi ->
-            {need with Gaining = (not need.Gaining)}
+            {need with GainState = (not need.GainState)}
         | x when x = need.Mini ->
-            {need with Gaining = (not need.Gaining)}
+            {need with GainState = (not need.GainState)}
         | _ -> need
     ) needMap
+
+/// This will return what the currently most important need is, given either a need or nothing.
+let getPriorityNeed (needMap : Map<NeedName,Need>) currentNeed =
+    match currentNeed with
+    // When none, we can just use any need as a starting value
+    | None -> Map.fold (fun x y z -> if x > z.CurrentValue then z.CurrentValue else x) (needMap |> Seq.head).Value.CurrentValue needMap 
+    | _ -> Map.fold (fun x y z -> if x-2.0 > z.CurrentValue then z.CurrentValue else x) (needMap.TryFind(currentNeed.Value).Value.CurrentValue) needMap // Make the existing value slightly "stickier"
+
+/// need a change task or task change needs or somesuch function now
 
 // Defaults for a given "class" to use
 let defaultRest =
@@ -66,29 +78,57 @@ let defaultRest =
         CurrentValue = 6.0;
         Maxi = 10.0;
         Mini = 0.0;
-        Gaining = true;
+        GainState = true;
         DecayDirection = Down;
         ChangeRate = 0.1;
     }
 
 let defaultBoredom =
     {
-        CurrentValue = 6.0;
+        CurrentValue = 5.0;
         Maxi = 10.0;
         Mini = 0.0;
-        Gaining = true;
+        GainState = true;
         DecayDirection = Up;
         ChangeRate = 0.1;
     }
 
 let defaultMapOfNeeds = Map.empty.Add(Rest,defaultRest).Add(Boredom,defaultBoredom)
 
+/// Going to start working on the task system now. I'm not sure if it should be a seperate module, cause it'd almost have to open this module regardless...
+/// So I'm just going to start work here and then pull it out later if I decide it would look better like that
+module TaskSystem =
+    type TaskNames = Wander | Nap
 
+    type TaskNeeds =
+        {
+            Gaining : NeedName list
+            Decaying : NeedName list
+        }
 
+    type Task =
+        | Started
+        | InProgress
+        | Completed
 
-let foo = Map.tryFind Rest defaultMapOfNeeds
-let unwrap bar =
-    match bar with
-    | None -> None
-    | _ -> bar.Value
+    // Now I think I'll make a list of all the tasks eventually but I'll start by declaring them discretely
+
+    let wander =
+        {
+            Gaining = [Boredom;]
+            Decaying = [Rest;]
+        }
+    let nap =
+        {
+            Gaining = [Rest;]
+            Decaying = [Boredom;]
+        }
+    let chinwag =
+        {
+            Gaining = [Rest; Boredom;]
+            Decaying = []
+        }
+
+    let changeTask needMap newTask =
+        needMap
 
--- C# experiment/GodotFs/PawnFs.fs
+++ C# experiment/GodotFs/PawnFs.fs
@@ -9,36 +9,12 @@ open NeedSystem
 type NeedNames = Rest = 0 | Boredom = 1
 type NeedTraits = OptimumValue = 0 | WorstValue = 1 | DecayDirection = 2 | DecayRate = 3 | CurrentDirection = 4
 
-// Building the custom types for a purely F# need system
-type DecayDirection = Up | Down
-type Need =
-    {
-        CurrentValue: float;
-        Maxi: float;
-        Mini: float;
-        Decaying: bool;
-        DecayDirection: DecayDirection;
-        ChangeRate: float;
-
-    }
-
 type PawnFs() as self =
     inherit Area2D()
 
     let startingNeeds = NeedSystem.defaultMapOfNeeds
 
     let mutable mutNeedMap = startingNeeds
-
-    let rest =
-        {
-        CurrentValue = 6.0;
-        Maxi = 10.0;
-        Mini = 0.0;
-        Decaying = true;
-        DecayDirection = Down;
-        ChangeRate = 0.1;
-        }
-    let mutable mutRest = rest
 
     let pathProviderNode = self.getNode "../Navigation2D"
 
@@ -52,54 +28,13 @@ type PawnFs() as self =
 
     //let wot = pathProviderNode.Value.saySomething
 
-    let changeNeedDir state (need: Need) =
-        match state with
-        | x when need.Decaying = x -> need
-        | _ -> {need with Decaying = state}
-
-    let changeNeedMag rate (need: Need) =
-        {need with ChangeRate = rate}
-
-    let changeNeedDirAndMag state rate (need: Need) =
-        changeNeedDir state need |> changeNeedMag rate
-
-    let updateNeed (need: Need) =
-        match (need.DecayDirection, need.Decaying) with
-        | (Up,true) | (Down,false) ->
-            let newValue = need.CurrentValue + need.ChangeRate
-            if newValue > need.Maxi then
-                {need with CurrentValue = need.Maxi}
-            else  
-                {need with CurrentValue = newValue}
-        | _ ->
-            let newValue = need.CurrentValue - need.ChangeRate
-            if newValue < need.Mini then
-                {need with CurrentValue = need.Mini}
-            else  
-                {need with CurrentValue = newValue}
-
-
-    let bounce (need: Need) =
-        match (need.CurrentValue, need.DecayDirection, need.Decaying) with
-        | (x, Up, true) | (x, Down, false) when x = need.Maxi ->
-            changeNeedDir (not need.Decaying) need
-        | (x, Up, false) | (x, Down, true) when x = need.Mini ->
-            changeNeedDir (not need.Decaying) need
-        | _ -> need
-
-
     // Currently only being called once every 60 frames or once per second.
     override this._PhysicsProcess(delta) =
-        mutRest <- updateNeed mutRest
-        mutRest <- bounce mutRest
-        GD.Print(mutRest.CurrentValue.ToString())
         mutNeedMap <- updateNeedMap mutNeedMap
         mutNeedMap <- mapBounce mutNeedMap
         Map.iter (fun (name : NeedName) (need : NeedSystem.Need ) -> GD.Print(need.CurrentValue.ToString())) mutNeedMap
-        
 
-
-    // Abstract (C# callable) Methods/functions
+    // Abstract (C# callable) Methods/funct ions
 
     //abstract member UpdateNeedTest: string -> unit
     //default this.UpdateNeedTest _ =
