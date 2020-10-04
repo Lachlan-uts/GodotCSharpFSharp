39c803162c26fc4d9ec947c51ff52d7286a67ed7
Lachlan-UTS <lachlan.d.wood@student.uts.edu.au>
2020-10-04 14:19:44 +1100

    Working on task/mission stickyness
    This is a really messy commit, I should have done more along the way but
    I've been a bit sporadic.

--- C# experiment/GodotFs/PathfindingFs.fs
+++ C# experiment/GodotFs/PathfindingFs.fs
@@ -23,4 +23,14 @@ let getRandomPointFromArea (possibleArea : Rect2) (validTiles : int[]) =
         randy.Value.RandfRange(possibleArea.Position.x, possibleArea.End.x),
         randy.Value.RandfRange(possibleArea.Position.y, possibleArea.End.y)
     )
-   
+
+let private fixDestination (nav2D : Lazy<Navigation2D>) targetVec =
+    //if getClosestValidTile nav2D targetVec - targetVec
+    if targetVec <> (getClosestValidTile nav2D targetVec) then
+        new Vector2(targetVec.x+0.01f,targetVec.y)
+    else
+        targetVec
+
+let calcPath (nav2D : Lazy<Navigation2D>) start destination =
+    let dest = fixDestination nav2D destination
+    nav2D.Value.GetSimplePath(start, destination, true)
--- C# experiment/GodotFs/PawnFs.fs
+++ C# experiment/GodotFs/PawnFs.fs
@@ -22,8 +22,14 @@ type PawnFs() as self =
 
     let mutable mutNeedMap = startingNeeds
     let mutable mutTaskMap = startingTasks //no use yet probably.
+    let mutable mutCurrentMission:TaskNames Option = None
+
+    let mutable mutCurrentPathA:Vector2 [] = Array.empty
+    let mutable mutCurrentPoint = 1
+    let mutable mutCurrentPathL:Vector2 list = List.empty
 
     let pathProviderNode = self.getNode "../Navigation2D"
+    let visAid = self.getNode "visualAid"
 
     let pathProviderCS =
         lazy(
@@ -35,28 +41,67 @@ type PawnFs() as self =
 
     //let wot = pathProviderNode.Value.saySomething
 
+    // the signal system
+    let mutable currentTask = 0
+    let handleActionEndingSignal word =
+        GD.Print(word)
+        currentTask <- currentTask+1
+
+    // Functions/actions that don't require iter in the main loop
+    let getPath destination =
+        //visAid.Value
+        PathProvider.calcPath pathProviderNode self.Position destination // Get the array of vector2 points
+        |> Array.toList // Turn it into a list
+        |> List.tail // Remove the first element because it's where this entity is and therefore is not needed.
+
+    // The missions/actions/task functions, probably want to pull them out to a distinct module
+    let walk (dir : Vector2) (speed : float32) (delta : float32) =
+        self.Translate(dir * speed * delta)
+
+    let chopTree () =
+        ()
+
+    let nap () =
+        ()
+
     // Currently only being called once every 60 frames or once per second.
     // Due to being called from within the C# script currently.
     override this._PhysicsProcess(delta) =
-        // I need to choose a need,
         let needList = (Map.map (fun k v -> v.CurrentValue) mutNeedMap) |> Map.toList |> (List.sortBy (fun (_,y) -> y))
+        let chooseMission currentMission (needL : (NeedName * float) list) =
+            match currentMission with
+            | None -> "get new task"
+            | Some x when (snd needL.Head) < 2.0 -> "get new task"
+            | _ -> "Stick with it"
+
+        //do tasky stuff here.
+        let action taskName =
+            match taskName with
+            | "task1" -> walk (new Vector2(0.0f,0.0f)) 10.0f delta
+            | "task2" -> chopTree ()
+            | _ -> nap ()
+
+        // I need to choose a need,
+        //let needList = (Map.map (fun k v -> v.CurrentValue) mutNeedMap) |> Map.toList |> (List.sortBy (fun (_,y) -> y))
         // then look at the list of tasks
         let newTask = getTaskFromPriorityNeed startingTasks (fst needList.Head)
         // then choose a task and swap to it or maintain current task
         mutNeedMap <- updateNeedsWithTask newTask mutNeedMap
         // then iterate needs.
-        mutNeedMap <- updateNeedMap mutNeedMap
+        mutNeedMap <- updateNeedMap mutNeedMap // Make this take a task and time?
         //mutNeedMap <- mapBounce mutNeedMap
         Map.iter (fun (name : NeedName) (need : NeedSystem.Need ) -> GD.Print(need.CurrentValue.ToString())) mutNeedMap
 
-    // Abstract (C# callable) Methods/funct ions
+    // Abstract (C# callable) Methods/functions
 
     //abstract member UpdateNeedTest: string -> unit
     //default this.UpdateNeedTest _ =
         ////mutRest <- updateNeed mutRest
         //GD.Print(mutRest.CurrentValue.ToString())
 
-    //abstract memeber ChangeTaskAdjustNeeds: Need
+    //abstract member ChangeTaskAdjustNeeds: Need
+
+
         
     abstract member ChooseWanderDest: Rect2 -> int[] -> Vector2
     default this.ChooseWanderDest (pArea : Rect2) (validTiles : int[]) =
